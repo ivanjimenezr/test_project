88e2512a7ad89dee616eda685a31ba8c09db7365
chiklete <chiklete@gmail.com>
2021-11-14 00:32:44 +0100

    terminamos con el code entero

--- main.js
+++ main.js
@@ -4,46 +4,50 @@ scoreElem = document.querySelector('span.score')
 // console.log(holesElem)
 // console.log(molesElem)
 // console.log(scoreElem)
-lastHole = document.querySelector('div.hole6')
+let lastHole;
+
+//Creamos la funcion para hacer un random del tiempo que salen los topos 
+function randomTime(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(holes){
+    const index  = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+
+    //prevent same hole from getting the same number
+    if (hole === lastHole){
+        return randomHole(holes);
+    }
+    lastHole = hole;
+    return hole;
+}
 
 function peep() {
-    let randon = Math.random() * (500 - 3000) + 3000
-    return Math.round(randon);
-   
-  }
+    const time = randomTime(500, 1000); //get a random time to determine how long mole should peep
+    const hole = randomHole(holes); //get the random hole from the randomHole function
+    hole.classList.add('up'); //add the CSS class so selected mole can "pop up"
+    setTimeout(() => {
+        hole.classList.remove('up'); //make the selected mole "pop down" after a random time
+        if(!timeUp) {
+            peep();
+        }
+    }, time);
+}
 
-  function startGame() {
+function startGame() {
     scoreBoard.textContent = 0;
     timeUp = false;
     score = 0;
     peep();
-    setTimeout(() => timeUp = true, 15000) 
-//muestra topos aleatoriamente durante 15 segundos
+    setTimeout(() => timeUp = true, 15000) //show random moles for 15 seconds
 }
 
+function wack(e){
+    if(!e.isTrusted) return; //** new thing I learned */
+    score++;
+    this.parentNode.classList.remove('up'); //this refers to item clicked
+    scoreBoard.textContent = score;
+}
 
-const addItem = () => {
-    const promesa = new Promise((resolve, reject) => {
-    //   if (!list) {
-    //     reject('No existe el array');
-    //   }
-  
-      setTimeout(function () {
-        timeUp = true
-        resolve(timeUp);
-      }, 3000);
-    });
-  
-    return promesa;
-  };
-  
-  
-  
-  addItem()
-    .then(() => {
-      console.log(`El listado final es`);
-    })
-    .catch((err) => {
-      throw new Error(err);
-    });
-
+moles.forEach(mole => mole.addEventListener('click', wack))
